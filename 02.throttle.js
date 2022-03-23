85f0aa20e4bb8ad43a3a65641055192cc7f827f0
wangshiyuan <wangshiyuan@navinfo.com>
2022-03-23 14:31:31 +0800

    防抖、节流

---
+++ 02.throttle.js
@@ -0,0 +1,12 @@
+// 节流，连续调用，只在指定的时间间隔内调用调用一次，减低频率即节流
+
+function throttle(func, wait) {
+    let start = Date.now();
+    return function() {
+        let now = Date.now();
+        if (now - start >= wait) {
+            func.apply(null, arguments);
+            start = now;
+        }
+    }
+}
