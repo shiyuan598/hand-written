f7d817fb281b7f355dad9fb2748c505179cc081c
wangshiyuan <wangshiyuan@navinfo.com>
2022-03-28 21:50:56 +0800

    异步任务调度

--- 17.asynctask.js
+++ 17.asynctask.js
@@ -1,25 +1,59 @@
+// 异步任务调度，多个异步任务按序执行
 let baseUrl = "https://jsonplaceholder.typicode.com/todos/";
 
 function ajax(url) {
     return fetch(url).then((res) => res.json());
 }
-function* generator() {
-    for (let i = 1; i < 100; i++) {
-        let data = yield ajax(baseUrl + i);
-        console.info(data);
+
+function solution1() {
+    let promise = Promise.resolve();
+    for (let i = 1; i <= 10; i++) {
+        promise = promise.then(() => {
+            return new Promise((resolve, reject) => {
+                ajax(baseUrl + i).then((data) => {
+                    console.info(data);
+                    resolve();
+                });
+            });
+        });
     }
 }
+// solution1();
 
-function run(generator) {
-    let it = generator();
-    function next(res) {
-        if (!res.done) {
-            res.value.then((data) => {
-                res = it.next(data);
-                next(res);
+function solution2() {
+    let arr = [...new Array(10)].map((v, i) => baseUrl + (i + 11));
+    arr.reduce((prev, cur) => {
+        return prev.then(() => {
+            return new Promise((resolve) => {
+                ajax(cur).then((data) => {
+                    console.info(data);
+                    resolve();
+                });
             });
+        });
+    }, Promise.resolve());
+}
+// solution2();
+
+function solution3() {
+    function* asyncQueue() {
+        for (let index = 21; index <= 30; index++) {
+            let data = yield ajax(baseUrl + index);
+            console.info(data);
         }
     }
-    next(it.next());
+
+    function run(gen) {
+        let it = gen();
+        (function next(value) {
+            let res = it.next(value);
+            if (!res.done) {
+                res.value.then((data) => {
+                    next(data);
+                });
+            }
+        })();
+    }
+    run(asyncQueue);
 }
-run(generator);
+solution3();
